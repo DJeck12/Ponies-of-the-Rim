00f833d99d49754f2330c0163b8df626c62a2f10
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2024-06-07 23:30:11 +0700

    Add Harmony BodyTypePatch & FurPatch.

---
+++ Source/Ponies of the Rim/Ponies of the Rim/Harmony_BodyTypePatch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace PoniesOfTheRim
+{
+    [StaticConstructorOnStartup]
+    public static class BodyTypePatch
+    {
+        static BodyTypePatch()
+        {
+            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(PawnGenerator), "GetBodyTypeFor"), null, new HarmonyMethod(typeof(BodyTypePatch).GetMethod("GenerateBodyTypePatch")));
+        }
+        [HarmonyPostfix]
+        public static void GenerateBodyTypePatch(Pawn pawn, ref BodyTypeDef __result)
+        {
+            if (pawn.def.defName.Contains("Pony_"))
+            {
+
+                if (ModsConfig.BiotechActive && pawn.DevelopmentalStage.Juvenile())
+                {
+                    if (pawn.DevelopmentalStage == DevelopmentalStage.Baby)
+                    {
+                        __result = Pony_DefOf.PonyBaby;
+                    }
+                    __result = Pony_DefOf.PonyChild;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Source/Ponies of the Rim/Ponies of the Rim/Harmony_FurPatch.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PoniesOfTheRim
+{
+    [StaticConstructorOnStartup]
+    public static class FurTypePatch
+    {
+        static FurTypePatch()
+        {
+            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(FurDef), "GetFurBodyGraphicPath"), null, new HarmonyMethod(typeof(FurTypePatch).GetMethod("FurPatch")));
+        }
+
+        [HarmonyPostfix]
+        public static void FurPatch(Pawn pawn, ref string __result, FurDef __instance)
+        {
+            if (pawn.def.defName.Contains("Pony_"))
+            {
+                __result = "Things/Races/Pony/Bodies/Furskin/Naked_Pony";
+            }
+            else
+            {
+                for (int i = 0; i < __instance.bodyTypeGraphicPaths.Count; i++)
+                {
+                    if (__instance.bodyTypeGraphicPaths[i].bodyType == pawn.story.bodyType)
+                    {
+                        __result = __instance.bodyTypeGraphicPaths[i].texturePath;
+                    }
+                }
+            }
+        }
+    }
+}    
+
+
+
+
+
+
+
+
+
