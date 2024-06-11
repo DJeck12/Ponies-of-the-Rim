f8c784ac4a5542bef5be8f9a5a14221e7ada8479
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2024-06-11 19:22:23 +0700

    Del FurPatch, Fix BodyTypePatch, return Furskin.xml with add PonyChild & PonyBaby bodytypes. (The offset needs to be corrected.)

--- Source/Ponies of the Rim/Ponies of the Rim/Harmony_BodyTypePatch.cs
+++ Source/Ponies of the Rim/Ponies of the Rim/Harmony_BodyTypePatch.cs
@@ -1,23 +1,24 @@
-﻿using HarmonyLib;
+﻿using AlienRace;
+using HarmonyLib;
 using RimWorld;
-using System;
+using System.Collections.Generic;
 using Verse;
 
 namespace PoniesOfTheRim
 {
     [StaticConstructorOnStartup]
-    public static class BodyTypePatch
+    public static class PonyBodyTypePatch
     {
-        static BodyTypePatch()
+        static PonyBodyTypePatch()
         {
-            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(PawnGenerator), "GetBodyTypeFor"), null, new HarmonyMethod(typeof(BodyTypePatch).GetMethod("GenerateBodyTypePatch")));
+            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(HarmonyPatches), "CheckBodyType"), null, new HarmonyMethod(typeof(PonyBodyTypePatch).GetMethod("BodyTypePatch")));
         }
+
         [HarmonyPostfix]
-        public static void GenerateBodyTypePatch(Pawn pawn, ref BodyTypeDef __result)
+        public static void BodyTypePatch(Pawn pawn, ref BodyTypeDef __result)
         {
             if (pawn.def.defName.Contains("Pony_"))
             {
-
                 if (ModsConfig.BiotechActive && pawn.DevelopmentalStage.Juvenile())
                 {
                     if (pawn.DevelopmentalStage == DevelopmentalStage.Baby)
@@ -28,8 +29,15 @@ namespace PoniesOfTheRim
                 }
             }
         }
+
     }
-}
+
+
+
+
+
+
+}    
 
 
 
