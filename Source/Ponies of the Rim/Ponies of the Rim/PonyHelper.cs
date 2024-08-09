6c08cfbfd6f8af030feab38cd588b93685ae81a7
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2024-08-10 03:17:23 +0700

    Add StylingStationPatch & PonyHelper.

---
+++ Source/Ponies of the Rim/Ponies of the Rim/PonyHelper.cs
@@ -0,0 +1,22 @@
+﻿using Verse;
+
+
+namespace PoniesOfTheRim
+{
+    internal static class PonyHelper
+    {
+        internal static bool IsPony(this Pawn pawn)
+        {
+            return pawn.kindDef.race.defName.Contains("Pony_");
+        }
+    }
+}    
+
+
+
+
+
+
+
+
+
