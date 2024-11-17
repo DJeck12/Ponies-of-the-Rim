bfaa8b83cbc48f576ed9360effc0df071807a3bc
DJeck12 <Anton_shum@mail.ru>
2024-11-17 16:05:34 +0300

    Code, art and sound changes, bug fixes, pre-release version

---
+++ Source/Ponies of the Rim/Gene_Random.cs
@@ -0,0 +1,17 @@
+﻿using Verse;
+
+namespace PoniesOfTheRim
+{
+    public class Gene_Random : Gene
+    {
+        public override void PostAdd()
+        {
+            base.PostAdd();
+            pawn.genes.AddGene(def.GetModExtension<GeneExtension>().genes.RandomElement(), pawn.genes.IsXenogene(this));
+            if (def.GetModExtension<GeneExtension>().removeAfter == true)
+            {
+                pawn.genes.RemoveGene(this);
+            }
+        }
+    }
+}
---
+++ Source/Ponies of the Rim/PonyHelper.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Verse;
+using static RimWorld.FleckMaker;
+
+
+namespace PoniesOfTheRim
+{
+    internal static class PonyHelper
+    {
+        internal static bool IsPony(this Pawn pawn)
+        {
+            return pawn.kindDef.race.defName.Contains("Pony_");
+        }
+        public static void PlaceHoofprint(Vector3 loc, Map map, float rot)
+        {
+            if (loc.ShouldSpawnMotesAt(map))
+            {
+                FleckCreationData dataStatic = GetDataStatic(loc, map, Pony_DefOf.Hoofprint, 0.5f);
+                dataStatic.rotation = rot;
+                map.flecks.CreateFleck(dataStatic);
+            }
+        }
+    }
+}
