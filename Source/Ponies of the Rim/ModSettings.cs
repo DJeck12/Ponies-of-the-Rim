2d1bc9e81db7c046ead0d788da41e7ca831e98d4
BuildByte <build_byte@outlook.com>
2024-12-02 21:32:06 +0100

    Adding Tooltips

--- Source/Ponies of the Rim/ModSettings.cs
+++ Source/Ponies of the Rim/ModSettings.cs
@@ -8,12 +8,25 @@ namespace PoniesOfTheRim
     public class PoniesOfTheRimSettingsData : ModSettings
     {
         //Keep Addisional Tabulators here to read hierarchy cleary
-        public bool classical; 
+        #region Classical
+        public bool classical;
             public bool ideology;
             public bool abilities;
             public bool prosthetics;
             public bool anthro;
             public bool enableRegularHorses;
+
+        public void ClassicalSettingsToggle(bool action)
+        {
+            ideology = action;
+            abilities = action;
+            prosthetics = action;
+            anthro = action;
+            enableRegularHorses = action;
+        }
+
+        public void ClassicalSettingsDefault() => ClassicalSettingsToggle(true);
+        #endregion
 
         /// <summary>
         /// This makes our settings file and sets them to default values.
@@ -45,12 +58,7 @@ namespace PoniesOfTheRim
 
             Listing_Standard listingStandard = new Listing_Standard();
             listingStandard.Begin(inRect);
-            listingStandard.Label("SettingsHeader".Translate());
-            //TODO: Make all button line horizontaly
-            if (listingStandard.ButtonText("SettingsEnableAll".Translate(), null, 0.2f))  SetAllData(true);
-            if (listingStandard.ButtonText("SettingsDisableAll".Translate(), null, 0.2f)) SetAllData(false);
-            if (listingStandard.ButtonText("SettingsDefulatAll".Translate(), null, 0.2f)) DefaultAllData();
-            listingStandard.GapLine(10);
+            Settingheader(listingStandard);
             listingStandard.Label("SettingsCore".Translate());
             ClassicalSettings(listingStandard);
             listingStandard.End();
@@ -59,61 +67,48 @@ namespace PoniesOfTheRim
             base.DoSettingsWindowContents(inRect);
         }
 
+        private void Settingheader(Listing_Standard listingStandard)
+        {
+            listingStandard.Label("SettingsHeader".Translate());
+            //TODO: Make all button line horizontaly
+            //var buttons = listingStandard.BeginSection(20,1,1);
+            if (listingStandard.ButtonText("SettingsEnableAll".Translate(), null, 0.2f)) SetAllData(true);
+            if (listingStandard.ButtonText("SettingsDisableAll".Translate(), null, 0.2f)) SetAllData(false);
+            if (listingStandard.ButtonText("SettingsDefulatAll".Translate(), null, 0.2f)) DefaultAllData();
+            listingStandard.GapLine(10);
+        }
+
         #region Classical
         private void ClassicalSettings(Listing_Standard listingStandard)
         {
-            listingStandard.CheckboxLabeled("└► " + "SettingsClassical".Translate(), ref settings.classical, "classicalToolTip".Translate(), 0, 1);
+            listingStandard.CheckboxLabeled("└► " + "SettingsClassical".Translate(), ref settings.classical, "SettingsToolTipClassical".Translate(), 0, 1);
             if (settings.classical)
             {
-             //! Uncomment ONLY HERE if is working
+                //! Uncomment ONLY HERE if is working
 
-                listingStandard.CheckboxLabeled("   ├► " + "SettingsIdeology".Translate(), ref settings.ideology, "addIdeologyToolTip".Translate(), 0, 1);
-                listingStandard.CheckboxLabeled("   ├► " + "SettingsAbilities".Translate(), ref settings.abilities, "addAbilitiesToolTip".Translate(), 0, 1);
-                listingStandard.CheckboxLabeled("   ├► " + "SettingsProsthetics".Translate(), ref settings.prosthetics, "addProstheticsToolTip".Translate(), 0, 1);
-                listingStandard.CheckboxLabeled("   ├► " + "SettingsAnthro".Translate(), ref settings.anthro, "addAnthroToolTip".Translate(), 0, 1);
-                listingStandard.CheckboxLabeled("   └► " + "SettingsEnableRegularHorses".Translate(), ref settings.enableRegularHorses, "addEnableRegularHorsesToolTip".Translate(), 0, 1);
+                listingStandard.CheckboxLabeled("   ├► " + "SettingsIdeology".Translate(), ref settings.ideology, "SettingsToolTipIdeology".Translate(), 0, 1);
+                listingStandard.CheckboxLabeled("   ├► " + "SettingsAbilities".Translate(), ref settings.abilities, "SettingsToolTipAbilities".Translate(), 0, 1);
+                listingStandard.CheckboxLabeled("   ├► " + "SettingsProsthetics".Translate(), ref settings.prosthetics, "SettingsToolTipProsthetics".Translate(), 0, 1);
+                listingStandard.CheckboxLabeled("   ├► " + "SettingsAnthro".Translate(), ref settings.anthro, "SettingsToolTipAnthro".Translate(), 0, 1);
+                listingStandard.CheckboxLabeled("   └► " + "SettingsEnableRegularHorses".Translate(), ref settings.enableRegularHorses, "SettingsToolTipEnableRegularHorses".Translate(), 0, 1);
             }
             else
             {
-                ClassicalSettingsDisabled();
+                settings.ClassicalSettingsToggle(false);
             }
-        }
-
-        private void ClassicalSettingsDefault()
-        {
-            settings.classical = true;
-            settings.ideology = true;
-            settings.abilities = true;
-            settings.prosthetics = true;
-            settings.anthro = true;
-            settings.enableRegularHorses = true;
-        }
-
-        private void ClassicalSettingsDisabled()
-        {
-            settings.ideology = false;
-            settings.abilities = false;
-            settings.prosthetics = false;
-            settings.anthro = false;
-            settings.enableRegularHorses = false;
         }
         #endregion
 
-        #region Main Buttons
+        #region Buttons
 
         private void DefaultAllData()
         {
-            ClassicalSettingsDefault();
+            settings.ClassicalSettingsDefault();
         }
 
         private void SetAllData(bool action)
         {
-            settings.classical = action;
-            settings.ideology = action;
-            settings.abilities = action;
-            settings.prosthetics = action;
-            settings.anthro = action;
-            settings.enableRegularHorses = action;
+            settings.ClassicalSettingsToggle(action);
         }
         #endregion
 
