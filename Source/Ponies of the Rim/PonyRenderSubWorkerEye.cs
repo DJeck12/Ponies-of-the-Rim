259242609cfd66b0dec416ab6595e669b9f95743
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2025-02-06 21:18:31 +0700

    Add fallbacks for change eyes colour
    and prosthetic.

---
+++ Source/Ponies of the Rim/PonyRenderSubWorkerEye.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Verse;
+using RimWorld;
+
+namespace PoniesOfTheRim
+{
+    public class PonyRenderSubWorkerEye : PawnRenderSubWorker
+    {
+        public override bool CanDrawNowSub(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (PonyHelper.IsPony(parms.pawn))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class NoPawnRenderSubWorkerEye : PawnRenderSubWorker
+    {
+        public override bool CanDrawNowSub(PawnRenderNode node, PawnDrawParms parms)
+        {
+            if (!PonyHelper.IsPony(parms.pawn))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    public class PonyRenderNodeProperties_Eye : PawnRenderNodeProperties
+    {
+        public PonyRenderNodeProperties_Eye()
+        {
+            visibleFacing = new List<Rot4>
+        {
+            Rot4.East,
+            Rot4.South,
+            Rot4.West
+        };
+            workerClass = typeof(PawnRenderNodeWorker_Eye);
+            nodeClass = typeof(PawnRenderNode_AttachmentHead);
+        }
+
+        public override void ResolveReferences()
+        {
+            skipFlag = RenderSkipFlagDefOf.Eyes;
+        }
+    }
+}
