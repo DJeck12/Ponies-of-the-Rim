77f4837ffb9dbd156543311cc0c34d8ef6174329
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2025-07-14 19:24:11 +0700

    Add PonyRawFoodFix.

---
+++ Source/Ponies of the Rim/Harmony_PonyRawFoodFix.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PoniesOfTheRim
+{
+    [StaticConstructorOnStartup]
+    public static class PonyRawFoodFix
+    {
+
+        static PonyRawFoodFix()
+        {
+            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(Thing), "Ingested"), null, new HarmonyMethod(typeof(PonyRawFoodFix).GetMethod("FoodFix")));
+        }
+
+
+        [HarmonyPostfix]
+        public static void FoodFix(Pawn ingester, float nutritionWanted, ThingWithComps __instance)
+        {
+            if (ingester.IsPony() && __instance.def.IsMeat)
+            {
+                ThoughtDef PonyAteMeatThought = DefDatabase<ThoughtDef>.GetNamed("Pony_AteMeat");
+                if (PonyAteMeatThought != null)
+                {
+                    ingester.needs.mood.thoughts.memories.TryGainMemory(PonyAteMeatThought);
+                }
+            }
+        }
+    }
+}
