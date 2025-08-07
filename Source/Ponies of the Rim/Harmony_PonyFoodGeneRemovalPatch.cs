925cb88cd014a080c89ff51908963b13c221d888
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2025-08-07 17:38:45 +0700

    Added carnivore and herbivore genes, added mod settings, fixed patch for CE.

---
+++ Source/Ponies of the Rim/Harmony_PonyFoodGeneRemovalPatch.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace PoniesOfTheRim
+{
+    [StaticConstructorOnStartup]
+    public static class PonyFoodGeneRemovalPatch
+    {
+        static PonyFoodGeneRemovalPatch()
+        {
+            new Harmony("Rimworld.Pony.PoniesOfTheRim").Patch(AccessTools.Method(typeof(PawnGenerator), nameof(PawnGenerator.GeneratePawn), [typeof(PawnGenerationRequest)]), null, new HarmonyMethod(typeof(PonyFoodGeneRemovalPatch).GetMethod("PonyFoodGeneRemovalForGeneratePawn")));
+        }
+
+        [HarmonyPostfix]
+        public static void PonyFoodGeneRemovalForGeneratePawn(Pawn __result)
+        {
+            if (!ModsConfig.BiotechActive || __result == null || __result.genes == null)
+            {
+                return;
+            }
+
+            var settings = LoadedModManager.GetMod<PoniesOfTheRimSettings>().GetSettings<PoniesOfTheRimSettingsData>();
+
+            if (!settings.enableFoodGenes && __result.IsPony())
+            {
+                Gene herbivoreGene = __result.genes.GetGene(Pony_DefOf.Pony_Herbivore);
+                if (herbivoreGene != null)
+                {
+                    __result.genes.RemoveGene(herbivoreGene);
+                }
+
+                Gene carnivoreGene = __result.genes.GetGene(Pony_DefOf.Pony_Carnivore);
+                if (carnivoreGene != null)
+                {
+                    __result.genes.RemoveGene(carnivoreGene);
+                }
+            }
+        }
+    }
+}
