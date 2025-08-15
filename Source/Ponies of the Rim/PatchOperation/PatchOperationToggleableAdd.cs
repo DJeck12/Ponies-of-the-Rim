2e4be8ed943e5f9a945680547ad5d85825675e02
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2025-08-16 01:07:28 +0700

    Adding PatchesForPonySettings for XML.

---
+++ Source/Ponies of the Rim/PatchOperation/PatchOperationToggleableAdd.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml;
+using Verse;
+
+namespace PoniesOfTheRim.PatchOperation
+{
+    public class PatchOperationToggleableAdd : Verse.PatchOperation
+    {
+        public string xpath;
+        public XmlContainer value;
+        public string settingId;
+        public bool defaultState;
+
+        protected bool CanRun()
+        {
+            bool enabled = PoniesOfTheRimSettings.settings.patchToggles.GetWithFallback(settingId, true);
+            return enabled;
+        }
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (!CanRun())
+            {
+                return true;
+            }
+
+            bool result = false;
+            try
+            {
+                foreach (object item in xml.SelectNodes(xpath))
+                {
+                    if (item is not XmlNode node)
+                    {
+                        continue;
+                    }
+                    XmlNode xmlNode = value.node;
+                    if (xmlNode.HasChildNodes)
+                    {
+                        foreach (XmlNode childNode in xmlNode.ChildNodes)
+                        {
+                            node.AppendChild(node.OwnerDocument.ImportNode(childNode, true));
+                        }
+                    }
+                    else
+                    {
+                        node.AppendChild(node.OwnerDocument.ImportNode(xmlNode, true));
+                    }
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Error applying patch: {ex.Message}");
+            }
+            return result;
+        }
+
+        public override void Complete(string modIdentifier)
+        {
+            base.Complete(modIdentifier);
+        }
+    }
+}
---
+++ Source/Ponies of the Rim/PatchOperation/PatchOperationToggleableSequence.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+namespace PoniesOfTheRim.PatchOperation
+{
+    public class PatchOperationToggleableSequence : PatchOperationSequence
+    {
+        public string settingId;
+        public List<Verse.PatchOperation> operations;
+        public Verse.PatchOperation lastFailedOperation;
+        public bool defaultState;
+
+        protected bool CanRun()
+        {
+            bool enabled = PoniesOfTheRimSettings.settings.patchToggles.GetWithFallback(settingId, true);
+            return enabled;
+        }
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (!CanRun())
+            {
+                return true;
+            }
+
+            bool result = true;
+            foreach (Verse.PatchOperation operation in operations)
+            {
+                result &= operation.Apply(xml);
+            }
+            return result;
+        }
+
+        public override string ToString()
+	    {
+		    int num = ((operations != null) ? operations.Count : 0);
+		    string text = string.Format("{0}(count={1}", base.ToString(), num);
+		    if (lastFailedOperation != null)
+		    {
+			    string text2 = text;
+			    Verse.PatchOperation patchOperation = lastFailedOperation;
+			    text = text2 + ", lastFailedOperation=" + ((patchOperation != null) ? patchOperation.ToString() : null);
+		    }
+		    return text + ")";
+	    }
+
+        public override void Complete(string modIdentifier)
+        {
+            base.Complete(modIdentifier);
+        }
+    }
+}
