5d0cee648ddba4a6fee106dce8dbefafc91d3dde
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2025-09-01 20:18:18 +0700

    Add PonyRandomBodyAddonCondition.

---
+++ Source/Ponies of the Rim/PonyRandomBodyAddonCondition.cs
@@ -0,0 +1,24 @@
+﻿using Verse;
+using System.Xml;
+using AlienRace.ExtendedGraphics;
+
+namespace PoniesOfTheRim
+{
+    public class PonyRandomBodyAddonCondition : Condition
+    {
+        public float chance = 1f;
+
+        public override bool Satisfied(ExtendedGraphicsPawnWrapper pawn, ref ResolveData data)
+        {
+            return Rand.Value < chance;
+        }
+
+        public override void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+            if (xmlRoot["chance"] != null)
+            {
+                chance = ParseHelper.ParseFloat(xmlRoot["chance"].InnerText);
+            }
+        }
+    }
+}
