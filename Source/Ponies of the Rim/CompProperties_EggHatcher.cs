6833fdea150c6f1af2acf510e1ea1dc090c55e06
MIIX333 <112000424+MIIX333@users.noreply.github.com>
2026-03-05 00:18:12 +0700

    Update
    It seems...

---
+++ Source/Ponies of the Rim/CompProperties_EggHatcher.cs
@@ -0,0 +1,14 @@
+﻿using Verse;
+
+namespace PoniesOfTheRim
+{
+    public class CompProperties_EggHatcher : CompProperties
+    {
+        public float daysToHatch = 18f;
+
+        public CompProperties_EggHatcher()
+        {
+            compClass = typeof(Comp_EggHatcher);
+        }
+    }
+}
